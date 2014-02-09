403bf3f613ea0fe505a510af9aab9c9361cf9244
Matthew Beauchesne <github@n1mjb.com>
2014-02-08 23:17:47 -0500

    Add initial test case.

--- QRZConnector/QRZConnectorTests/QRZConnectorTests.m
+++ QRZConnector/QRZConnectorTests/QRZConnectorTests.m
@@ -3,10 +3,13 @@
 //  QRZConnectorTests
 //
 //  Created by Matthew on 2/8/14.
-//  Copyright (c) 2014 N1MJB. All rights reserved.
+//  Copyright (c) 2014 Matthew J Beauchesne. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+
+#import "QRZConnector.h"
+#import "QRZContainer.h"
 
 @interface QRZConnectorTests : XCTestCase
 
@@ -26,9 +29,48 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testRetrieve
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    
+    char str[100];
+    
+    printf("Enter a user name: ");
+    scanf("%s", str);
+    
+    NSString *userName = [NSString stringWithUTF8String:str];
+
+    printf("Enter the password: ");
+    scanf("%s", str);
+    
+    NSString *passwd = [NSString stringWithUTF8String:str];
+
+    printf("Enter a callsign: ");
+    scanf("%s", str);
+    
+    NSString *callSign = [NSString stringWithUTF8String:str];
+    
+    // Create the retriever object and login to get key.
+    QRZConnector *qrzRetriever = [[QRZConnector alloc] initWithUserName:userName andPassword:passwd];
+    
+    // Retrieve QRZ data from website.
+    NSMutableDictionary *qrzData = [qrzRetriever getQRZData:callSign];
+    
+    QRZContainer *qrzObj = [[QRZContainer alloc] initWithDictionary:qrzData];
+    NSLog(@"call = %@", [qrzObj call]);
+    NSLog(@"licenseClass = %@", [qrzObj licenseClass]);
+    NSLog(@"firstName = %@", [qrzObj firstName]);
+    NSLog(@"lastName = %@", [qrzObj lastName]);
+    NSLog(@"addr1 = %@", [qrzObj addr1]);
+    NSLog(@"addr2 = %@", [qrzObj addr2]);
+    NSLog(@"state = %@", [qrzObj state]);
+    NSLog(@"county = %@", [qrzObj county]);
+    NSLog(@"zip = %@", [qrzObj zip]);
+    NSLog(@"country = %@", [qrzObj country]);
+    NSLog(@"lat = %@", [qrzObj lat]);
+    NSLog(@"lon = %@", [qrzObj lon]);
+    NSLog(@"grid = %@", [qrzObj grid]);
+    
+    XCTAssertEqualObjects([qrzObj call], [callSign uppercaseString], @"Call sign doesn't match: %@ <> %@", [qrzObj call], [callSign uppercaseString]);
 }
 
 @end
